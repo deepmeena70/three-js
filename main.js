91ff8f4fde846070b95ac3622d59573d188f7e4e
Deep <45520903+deepmeena70@users.noreply.github.com>
2023-02-07 04:55:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -124,8 +124,10 @@ import WebGL from 'capabilities'; //check capabilites of broser by checking supp
     const vid = document.querySelector('video');
     if (vid.classList.contains('disable')) {
       vid.removeAttribute('class', 'disable');
+      vidBtn.innerHTML = 'Enable Video';
     } else {
       vid.setAttribute('class', 'disable');
+      vidBtn.innerHTML = 'Disable Video';
     }
   };
 
